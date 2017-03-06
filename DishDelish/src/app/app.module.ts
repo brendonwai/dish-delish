fe0c9294bc4ae5960082b880de8c6c07d0946ab4
Brendon Wai <macbrendon95@gmail.com>
2017-03-06 15:08:20 -0800

    Configured app to link to Firebase

--- DishDelish/src/app/app.module.ts
+++ DishDelish/src/app/app.module.ts
@@ -2,8 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyD8LaKHrqt12K-H1XXf2MfPt6BAEy6xCA4",
+  authDomain: "dishdelish-f559d.firebaseapp.com",
+  databaseURL: "https://dishdelish-f559d.firebaseio.com",
+  storageBucket: "dishdelish-f559d.appspot.com",
+  messagingSenderId: "284773768656"
+};
 
 @NgModule({
   declarations: [
@@ -12,7 +21,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
