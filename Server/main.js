a22418d2e9529a8bcf2492196e8e64ba7d1ea917
Brendon Wai <macbrendon95@gmail.com>
2017-03-12 23:08:24 -0700

    Added twitter integration to server

--- Server/main.js
+++ Server/main.js
@@ -1,5 +1,6 @@
 var http = require("http");
 var admin = require("firebase-admin");
+var Twitter = require("twitter");
 var serviceAccount = require("./dishdelish-f559d-firebase-adminsdk-0r6d5-0420999143.json");
 
 admin.initializeApp({
@@ -17,6 +18,21 @@ var server = http.createServer(function(req, res){
 
 var db = admin.database();
 var ref = db.ref("/cities");
+
+//twitter here
+var client = new Twitter({
+  consumer_key: 'YS0ozR5l4hMjraZfyYXJlbta0',
+  consumer_secret: 'iDjpy6Hz5qaRdrJtVkvG2OfRDlsapZw2FpDEBJi6eOgjZClXmR',
+  access_token_key: '841140793494581248-gFz1TsVWDKi5kr9s3uG4cCa3vAUaBCY',
+  access_token_secret: 'i0O9cCWp2xyPH9vnyidGalv7cdbTuXXIJJD64VlM3EWFd'
+});
+var params = {screen_name: 'nodejs'};
+client.get('search/tweets', {q:'dick'}, function(error, tweets, response){
+  if (!error){
+    console.log(tweets);
+  }
+})
+
 //addNewEntry("San francisco", "chicken");
 //removeEntry("San francisco");
 server.listen(8080);
