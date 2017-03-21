277e29cd4e6164aa4a16a541cb9a7d8f46f3dcb3
Brendon Wai <macbrendon95@gmail.com>
2017-03-20 19:41:57 -0700

    Fixed filtered food.txt, removed some comment code

--- Server/test.js
+++ Server/test.js
@@ -1,2 +1,6 @@
-str = "hello world !";
-console.log(str.replace(/ /g,''));
+var fs = require('fs');
+
+foodbase = fs.readFileSync('FOOD.txt').toString().split("\n");
+
+console.log(foodbase[5].replace(/ /g,'').toLowerCase());
+console.log('abalone' == foodbase[5].replace(/ /g,'').toLowerCase());
